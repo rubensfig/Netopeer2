bff653de9357d4616489dc622c870cbd7a074241
rubensfig <r.figueiredo.52@gmail.com>
2018-01-15 16:35:12 +0000

    add etcd functions

--- server/cetcd-connector.c
+++ server/cetcd-connector.c
@@ -1,21 +1,32 @@
-#include "libraries/cetcd/cetcd.h"
-#include "libraries/cetcd/cetcd_array.h"
+#include "cetcd-connector.h"
 
-void init_etcd_connection(cetcd_array* resp) {
+int etcd_get(char* dir, cetcd_client cli) {
+
+    cetcd_response *resp;
+    resp = cetcd_get(&cli, "/");
+    cetcd_response_print(resp);
+
+    if(resp->err) {
+	return -1;
+    }
+}
+
+int etcd_data_parse(struct nc_session *ncs) {
+	
+	
+}
+
+cetcd_client* init_etcd_connection() {
+	 
     cetcd_array addrs;
+    cetcd_client cli;
 
     cetcd_array_init(&addrs, 3);
     cetcd_array_append(&addrs, "http://etcd0:2379");
 
-    cetcd_client cli;
     cetcd_client_init(&cli, &addrs);
 
-    resp = cetcd_get(&cli, "/");
-
-
+    etcd_get("/", cli);
+    return &cli;
 }
 
-
-void etcd_data_parse(struct nc_pollsession *ps) {
-    
-}
