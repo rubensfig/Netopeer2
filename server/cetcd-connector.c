b48b68a31522c7c86816a6384f459afbae6e134d
rubensfig <r.figueiredo.52@gmail.com>
2018-01-15 13:43:15 +0000

    linked

--- server/cetcd-connector.c
+++ server/cetcd-connector.c
@@ -6,7 +6,15 @@ void init_etcd_connection(cetcd_array* resp) {
 
     cetcd_array_init(&addrs, 3);
     cetcd_array_append(&addrs, "http://etcd0:2379");
+
+    cetcd_client cli;
+    cetcd_client_init(&cli, &addrs);
+
+    resp = cetcd_get(&cli, "/");
+
+
 }
+
 
 void etcd_data_parse(struct nc_pollsession *ps) {
     
